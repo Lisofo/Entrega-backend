628d7e57e021d2a9106eb9618908fd5ceac00863
Santiago <santicg01@gmail.com>
2025-06-14 12:29:49 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import productsRouter from './routes/products.routes.js';
-import cartsRouter from './routes/cartsRoutes.js';
+import cartsRouter from './routes/carts.routes.js';
 
 const app = express();
 
